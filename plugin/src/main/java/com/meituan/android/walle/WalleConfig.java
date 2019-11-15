04d9bb7e48a2806cdbdc85d8778542d3a4e9ce36
zeus <zeuszeus>
2019-11-15 17:33:38 +0800

    Initial commit

---
+++ plugin/src/main/java/com/meituan/android/walle/WalleConfig.java
@@ -0,0 +1,120 @@
+package com.meituan.android.walle;
+
+import java.util.List;
+import java.util.Map;
+
+public class WalleConfig {
+
+    /**
+     * strategy:
+     * 1. ifNone (默认适用此策略) : 仅当对应channel没有extraInfo时生效
+     * 2. always : 所有channel都生效，channel中extraInfo的key与defaultExtraInfo重复时，覆盖defaultExtraInfo中的内容。
+     */
+    public static final String STRATEGY_IF_NONE = "ifNone";
+    public static final String STRATEGY_ALWAYS = "always";
+    private String defaultExtraInfoStrategy = STRATEGY_IF_NONE;
+
+    public String getDefaultExtraInfoStrategy() {
+        return defaultExtraInfoStrategy;
+    }
+
+    public void setDefaultExtraInfoStrategy(String defaultExtraInfoStrategy) {
+        this.defaultExtraInfoStrategy = defaultExtraInfoStrategy;
+    }
+
+    private Map<String, String> defaultExtraInfo;
+
+    public List<ChannelInfo> getChannelInfoList() {
+        return channelInfoList;
+    }
+
+    public void setChannelInfoList(List<ChannelInfo> channelInfoList) {
+        this.channelInfoList = channelInfoList;
+    }
+
+    private List<ChannelInfo> channelInfoList;
+
+    public Map<String, String> getDefaultExtraInfo() {
+        return defaultExtraInfo;
+    }
+
+    public void setDefaultExtraInfo(Map<String, String> defaultExtraInfo) {
+        this.defaultExtraInfo = defaultExtraInfo;
+    }
+
+    public static class ChannelInfo {
+        private String channel;//渠道
+        private String alias;//别名
+        private String appLabel;//app名称
+        private String appID;//自定义applicationId
+        private String appIcon;
+        private String roundIcon;
+
+        public String getRoundIcon() {
+            return roundIcon;
+        }
+
+        public void setRoundIcon(String roundIcon) {
+            this.roundIcon = roundIcon;
+        }
+
+        public String getAppName() {
+            return appLabel;
+        }
+
+        public void setAppName(String appLabel) {
+            this.appLabel = appLabel;
+        }
+
+        public String getAppID() {
+            return appID;
+        }
+
+        public void setAppID(String appID) {
+            this.appID = appID;
+        }
+
+        public String getAppIcon() {
+            return appIcon;
+        }
+
+        public void setAppIcon(String appIcon) {
+            this.appIcon = appIcon;
+        }
+
+        public String getAlias() {
+            return alias;
+        }
+
+        public void setAlias(String alias) {
+            this.alias = alias;
+        }
+
+        /**
+         * 强制声明不使用defaultExtraInfo参数
+         */
+        private boolean excludeDefaultExtraInfo = false;
+
+        public boolean isExcludeDefaultExtraInfo() {
+            return excludeDefaultExtraInfo;
+        }
+
+        private Map<String, String> extraInfo;
+
+        public String getChannel() {
+            return channel;
+        }
+
+        public void setChannel(String channel) {
+            this.channel = channel;
+        }
+
+        public Map<String, String> getExtraInfo() {
+            return extraInfo;
+        }
+
+        public void setExtraInfo(Map<String, String> extraInfo) {
+            this.extraInfo = extraInfo;
+        }
+    }
+}
